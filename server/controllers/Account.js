2bddf223c74838d63288dc7d9d5716fed7df2046
katie-b9030 <klb9030@rit.edu>
2025-04-16 13:28:54 -0400

    beginning project 2

---
+++ server/controllers/Account.js
@@ -0,0 +1,76 @@
+const models = require("../models");
+
+const { Account } = models;
+
+const loginPage = (req, res) => res.render("login");
+
+const logout = (req, res) => {
+  req.session.destroy();
+  res.redirect("/");
+};
+
+const login = (req, res) => {
+  const username = `${req.body.username}`;
+  const pass = `${req.body.pass}`;
+
+  if (!username || !pass) {
+    return res.status(400).json({ error: "All fields are required!" });
+  }
+
+  return Account.authenticate(username, pass, (err, account) => {
+    if (err || !account) {
+      return res.status(401).json({ error: "Wrong username or password!" });
+    }
+
+    req.session.account = Account.toAPI(account);
+
+    return res.json({ redirect: "/log" });
+  });
+};
+
+const signup = async (req, res) => {
+  const username = `${req.body.username}`;
+  const pass = `${req.body.pass}`;
+  const pass2 = `${req.body.pass2}`;
+  const firstName = `${req.body.firstName}`;
+  const lastName = `${req.body.lastName}`;
+
+  const name = `${firstName} ${lastName}`;
+
+  if (!username || !pass || !pass2 || !name) {
+    return res.status(400).json({ error: "All fields are required!" });
+  }
+
+  if (pass !== pass2) {
+    return res.status(400).json({ error: "Passwords do not match!" });
+  }
+
+  try {
+    const hash = await Account.generateHash(pass);
+    const newAccount = new Account({
+      username,
+      password: hash,
+      name,
+    });
+    await newAccount.save();
+
+    const accountData = Account.toAPI(newAccount);
+    console.log("✅ Session account being set:", accountData); // <--- check this
+    req.session.account = accountData;
+
+    return res.json({ redirect: "/log" });
+  } catch (err) {
+    console.log(err);
+    if (err.code === 11000) {
+      return res.status(400).json({ error: "Username already in use!" });
+    }
+    return res.status(500).json({ error: "An error occurred!" });
+  }
+};
+
+module.exports = {
+  loginPage,
+  login,
+  logout,
+  signup,
+};
---
+++ server/router.js
@@ -0,0 +1,35 @@
+const controllers = require("./controllers");
+const mid = require("./middleware");
+
+const router = (app) => {
+  app.get(
+    "/login",
+    mid.requiresSecure,
+    mid.requiresLogout,
+    controllers.Account.loginPage
+  );
+  app.post(
+    "/login",
+    mid.requiresSecure,
+    mid.requiresLogout,
+    controllers.Account.login
+  );
+
+  app.post(
+    "/signup",
+    mid.requiresSecure,
+    mid.requiresLogout,
+    controllers.Account.signup
+  );
+
+  app.get("/logout", mid.requiresLogin, controllers.Account.logout);
+
+  app.get(
+    "/",
+    mid.requiresSecure,
+    mid.requiresLogout,
+    controllers.Account.loginPage
+  );
+};
+
+module.exports = router;
